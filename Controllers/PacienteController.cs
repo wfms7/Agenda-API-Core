f16f5538ae60c702f55b253a2fcaf7c661658420
Walter Fernando Moraes Santos <wfms7@hotmail.com>
2022-04-22 14:11:18 -0300

    Criando Controllers

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,88 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebNotebook.Data.Dto;
+using WebNotebook.Data.Dto.Paciente;
+using WebNotebook.Services;
+
+namespace WebNotebook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PacienteController : ControllerBase
+    {
+
+        private PacienteService _pacienteService;
+
+        public PacienteController(PacienteService pacienteService)
+        {
+            _pacienteService = pacienteService;
+        }
+
+        [HttpPost]
+
+        public IActionResult AdicionaPaciente([FromBody] CreatePacienteDto Dto)
+        {
+            ReadPacienteDto readPaciente = _pacienteService.AdionaPaciente(Dto);
+
+            return CreatedAtAction(nameof(ReucperaPacientePorId), new { Id = readPaciente.Id }, readPaciente);
+
+        }
+
+
+        [HttpGet]
+
+        public IActionResult RecuperarPaceinte([FromQuery] string Nome = null, int skip = 0 ,int take =10)
+        {
+           ( List<ReadPacienteDto> readDto , int countPaciente) = _pacienteService.RecuperarPaciente(Nome,skip,take);
+            if (readDto != null)
+            {
+                return Ok( new { data = readDto, countPaciente });
+            }
+            return NotFound();
+
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult ReucperaPacientePorId(int id)
+        {
+
+            ReadPacienteDto readPaciente = _pacienteService.ReucperaPacientePorId(id);
+            if (readPaciente != null) { return Ok(readPaciente); }
+            return NotFound();
+
+
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult AtulizaPaciente (int id, [FromBody] UpdatePacienteDto pacienteDto)
+        {
+
+            Result resultado = _pacienteService.AtualizaPaciente(id, pacienteDto);
+            if (resultado.IsFailed) 
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeletaPaciente(int id)
+        {
+            Result resultado = _pacienteService.DeletaPaciente(id);
+            if (resultado.IsFailed)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+    }
+}
---
+++ Controllers/TelefoneController.cs
@@ -0,0 +1,86 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebNotebook.Data.Dto.Telefone;
+using WebNotebook.Services;
+
+namespace WebNotebook.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TelefoneController : ControllerBase
+    {
+        private TelefoneService _telefoneService;
+
+        public TelefoneController(TelefoneService telefoneService)
+        {
+            _telefoneService = telefoneService;
+        }
+
+
+        [HttpPost]
+
+        public IActionResult CreateTelefone([FromBody] List<CreateTelefoneDto> dto)
+        {
+            try
+            {
+                List<ReadTelefoneDto> readDto = _telefoneService.CreateTelefone(dto);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound(ex.Message.ToString());
+            }
+
+        }
+
+        [HttpGet]
+
+        public IActionResult RecuperaTelefone([FromQuery] string Telefone = null, [FromQuery] int id=-1)
+
+        {
+            List<ReadTelefoneDto> readDto = _telefoneService.RecuperaTelefone(Telefone,id);
+            if (readDto != null)
+            {
+                return Ok(readDto);
+            }
+            return NotFound();
+        }
+
+
+        
+
+
+        [HttpPut("{id}")]
+
+        public Result AlterarTelefone(int id,UpdateTelefoneDto dto)
+        {
+            Result resultado = _telefoneService.AtulizaTelefone(id,dto);
+            if (resultado.IsFailed)
+            {
+                return Result.Fail(resultado.IsFailed.ToString());
+            }
+
+            return Result.Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public Result DeletaTelefone(int id)
+        {
+            Result resultado = _telefoneService.DeletaTelefone(id);
+            if (resultado.IsFailed)
+            {
+                return Result.Fail(resultado.IsFailed.ToString());
+            }
+
+            return Result.Ok();
+        }
+
+    }
+}
